cfe8ff15252f59ec79825c516423b92b597e7b52
Andrew Martin <andrew.thaddeus@gmail.com>
2017-08-11 09:49:41 -0400

    initial commit

---
+++ src/Test/QuickCheck/Classes.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE MagicHash #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Test.QuickCheck.Classes
+  ( primProps
+  , monoidProps
+  ) where
+
+import Test.QuickCheck
+import Data.Primitive
+import Data.Primitive.PrimArray
+import Data.Proxy
+import Control.Monad.ST
+import GHC.Ptr (Ptr(..))
+import Data.Primitive.Addr (Addr(..))
+import Foreign.Marshal.Alloc
+import System.IO.Unsafe
+import Data.Semigroup (Semigroup)
+import qualified Data.Semigroup as SG
+import qualified GHC.OldList as L
+
+semigroupProps :: (Semigroup a, Eq a, Arbitrary a, Show a) => Proxy a -> [(String,Property)]
+semigroupProps p =
+  [ ("Associative", semigroupAssociative p)
+  ]
+
+monoidProps :: (Monoid a, Eq a, Arbitrary a, Show a) => Proxy a -> [(String,Property)]
+monoidProps p =
+  [ ("Associative", monoidAssociative p)
+  , ("Left Identity", monoidLeftIdentity p)
+  , ("Right Identity", monoidRightIdentity p)
+  ]
+
+primProps :: (Prim a, Eq a, Arbitrary a, Show a) => Proxy a -> [(String,Property)]
+primProps p =
+  [ ("Set-Get ByteArray (you get back what you put in)", primSetGetByteArray p)
+  , ("Get-Set ByteArray (putting back what you got out has no effect)", primGetSet p)
+  , ("Set-Set ByteArray (setting twice is same as setting once)", primSetSet p)
+  , ("Set-Get Addr (you get back what you put in)", primSetGetAddr p)
+  ]
+
+semigroupAssociative :: forall a. (Semigroup a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+semigroupAssociative _ = property $ \(a :: a) b c -> a SG.<> (b SG.<> c) == (a SG.<> b) SG.<> c
+
+monoidAssociative :: forall a. (Monoid a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+monoidAssociative _ = property $ \(a :: a) b c -> mappend a (mappend b c) == mappend (mappend a b) c
+
+monoidLeftIdentity :: forall a. (Monoid a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+monoidLeftIdentity _ = property $ \(a :: a) -> mappend mempty a == a
+
+monoidRightIdentity :: forall a. (Monoid a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+monoidRightIdentity _ = property $ \(a :: a) -> mappend a mempty == a
+
+primSetGetByteArray :: forall a. (Prim a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+primSetGetByteArray _ = property $ \(a :: a) len -> (len > 0) ==> do
+  ix <- choose (0,len - 1)
+  return $ runST $ do
+    arr <- newPrimArray len
+    writePrimArray arr ix a
+    a' <- readPrimArray arr ix
+    return (a == a')
+
+primGetSet :: forall a. (Prim a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+primGetSet _ = property $ \(as :: [a]) -> (not (L.null as)) ==> do
+  let arr1 = fromList as :: PrimArray a
+      len = L.length as
+  ix <- choose (0,len - 1)
+  arr2 <- return $ runST $ do
+    marr <- newPrimArray len
+    copyPrimArray marr 0 arr1 0 len
+    a <- readPrimArray marr ix
+    writePrimArray marr ix a
+    unsafeFreezePrimArray marr
+  return (arr1 == arr2)
+
+primSetSet :: forall a. (Prim a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+primSetSet _ = property $ \(a :: a) (as :: [a]) -> (not (L.null as)) ==> do
+  let arr1 = fromList as :: PrimArray a
+      len = L.length as
+  ix <- choose (0,len - 1)
+  (arr2,arr3) <- return $ runST $ do
+    marr2 <- newPrimArray len
+    copyPrimArray marr2 0 arr1 0 len
+    writePrimArray marr2 ix a
+    marr3 <- newPrimArray len
+    copyMutablePrimArray marr3 0 marr2 0 len
+    arr2 <- unsafeFreezePrimArray marr2
+    writePrimArray marr3 ix a
+    arr3 <- unsafeFreezePrimArray marr3
+    return (arr2,arr3)
+  return (arr2 == arr3)
+
+primSetGetAddr :: forall a. (Prim a, Eq a, Arbitrary a, Show a) => Proxy a -> Property
+primSetGetAddr _ = property $ \(a :: a) len -> (len > 0) ==> do
+  ix <- choose (0,len - 1)
+  return $ unsafePerformIO $ do
+    ptr@(Ptr addr#) :: Ptr a <- mallocBytes (len * sizeOf (undefined :: a))
+    let addr = Addr addr#
+    writeOffAddr addr ix a
+    a' <- readOffAddr addr ix
+    free ptr
+    return (a == a')
+
---
+++ test/Spec.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+import Test.QuickCheck
+import Data.Proxy
+import Data.Word
+import Data.Int
+import Control.Monad
+import Data.Primitive
+import Data.Foldable
+import Data.Monoid (Sum)
+
+import Test.QuickCheck.Classes
+
+main :: IO ()
+main = do
+  putStrLn "Testing properties for common typeclasses"
+  r <- flip foldlMapM allPropsApplied $ \(typeName,properties) -> do
+    putStrLn $ "------------"
+    putStrLn $ "-- " ++ typeName
+    putStrLn $ "------------"
+    flip foldlMapM properties $ \(name,p) -> do
+      putStrLn name
+      r <- quickCheckResult p
+      return $ case r of
+        Success _ _ _ -> Good
+        _ -> Bad
+  putStrLn ""
+  case r of
+    Good -> putStrLn "All tests succeeded"
+    Bad -> putStrLn "One or more tests failed"
+
+data Status = Bad | Good
+
+instance Monoid Status where
+  mempty = Good
+  mappend Good x = x
+  mappend Bad _ = Bad
+
+allPropsApplied :: [(String,[(String,Property)])]
+allPropsApplied = 
+  [ ("Word8",allProps (Proxy :: Proxy Word8))
+  , ("Int16",allProps (Proxy :: Proxy Int16))
+  , ("Int",allProps (Proxy :: Proxy Int))
+  , ("Word",allProps (Proxy :: Proxy Word))
+  , ("Int64",allProps (Proxy :: Proxy Int64))
+  ]
+
+allProps :: forall a. (Num a, Prim a, Eq a, Arbitrary a, Show a) => Proxy a -> [(String,Property)]
+allProps p = concat
+  [ primProps p
+  , monoidProps (Proxy :: Proxy (Sum a))
+  ]
+
+foldlMapM :: (Foldable t, Monoid b, Monad m) => (a -> m b) -> t a -> m b
+foldlMapM f = foldlM (\b a -> fmap (mappend b) (f a)) mempty
+
