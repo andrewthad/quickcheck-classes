bee7b9867733dcbd20f4c92e9dc0be790b048060
chessai <chessai1996@gmail.com>
2019-05-16 19:37:43 -0400

    add applyAssociativity

--- src/Test/QuickCheck/Classes/Apply.hs
+++ src/Test/QuickCheck/Classes/Apply.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 #if HAVE_QUANTIFIED_CONSTRAINTS
@@ -25,10 +26,20 @@ import Test.QuickCheck.Property (Property)
 import Test.QuickCheck.Classes.Common
 import Test.QuickCheck.Classes.Compat (eq1)
 
+type ApplyProp proxy f =
+#if HAVE_QUANTIFIED_CONSTRAINTS
+  (FunctorApply.Apply f, forall x. Eq x => Eq (f x), forall x. Show x => Show (f x), forall x. Arbitrary x => Arbitrary (f x)) 
+#else
+  (FunctorApply.Apply f, Eq1 f, Show1 f, Arbitrary1 f)
+#endif
+  => proxy f -> Property
+
 -- | Tests the following alt properties:
 --
 -- [/LiftF2 (1)/]
 --   @('FunctorApply.<.>') ≡ 'FunctorApply.liftF2' 'id'@
+-- [/Associativity/]
+--   @'fmap' ('.') u 'FunctorApply.<.>' v 'FunctorApply.<.>' w ≡ u 'FunctorApply.<.>' (v 'FunctorApply.<.>' w)@
 applyLaws ::
 #if HAVE_QUANTIFIED_CONSTRAINTS
   (FunctorApply.Apply f, forall a. Eq a => Eq (f a), forall a. Show a => Show (f a), forall a. Arbitrary a => Arbitrary (f a))
@@ -38,16 +49,18 @@ applyLaws ::
   => proxy f -> Laws
 applyLaws p = Laws "Apply"
   [ ("LiftF2 part 1", applyLiftF2_1 p)
+  , ("Associativity", applyAssociativity p)
   ]
 
-applyLiftF2_1 :: forall proxy f. 
-#if HAVE_QUANTIFIED_CONSTRAINTS
-  (FunctorApply.Apply f, forall a. Eq a => Eq (f a), forall a. Show a => Show (f a), forall a. Arbitrary a => Arbitrary (f a))
-#else
-  (FunctorApply.Apply f, Eq1 f, Show1 f, Arbitrary1 f)
-#endif
-  => proxy f -> Property
+applyLiftF2_1 :: forall proxy f. ApplyProp proxy f
 applyLiftF2_1 _ = property $ \(Apply (f' :: f QuadraticEquation)) (Apply (x :: f Integer)) ->
   let f = fmap runQuadraticEquation f'
   in eq1 (FunctorApply.liftF2 id f x) (f FunctorApply.<.> x)
+
+applyAssociativity :: forall proxy f. ApplyProp proxy f
+applyAssociativity _ = property $ \(Apply (u' :: f QuadraticEquation)) (Apply (v' :: f QuadraticEquation)) (Apply (w :: f Integer)) ->
+  let u = fmap runQuadraticEquation u'
+      v = fmap runQuadraticEquation v'
+   in eq1 (fmap (.) u FunctorApply.<.> v FunctorApply.<.> w) (u FunctorApply.<.> (v FunctorApply.<.> w))
+
 #endif
